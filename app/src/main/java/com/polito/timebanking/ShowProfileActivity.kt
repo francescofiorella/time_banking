7a13be526b237e89205b552370c5eac59d0d1027
Antonello Caputo s302879 <s302879@studenti.polito.it>
2022-03-29 11:22:08 +0200

    no toolbar

--- app/src/main/java/com/polito/timebanking/ShowProfileActivity.kt
+++ app/src/main/java/com/polito/timebanking/ShowProfileActivity.kt
@@ -7,5 +7,6 @@ class ShowProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
+
     }
 }
