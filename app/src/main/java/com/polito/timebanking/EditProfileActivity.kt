c09aa67501aa32145e86965bc0c613f8a63d8942
FRANCESCO FIORELLA s303922 <s303922@studenti.polito.it>
2022-04-07 17:08:31 +0200

    edit profile implemented

--- app/src/main/java/com/polito/timebanking/EditProfileActivity.kt
+++ app/src/main/java/com/polito/timebanking/EditProfileActivity.kt
@@ -1,22 +1,49 @@
 package com.polito.timebanking
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
+import android.widget.EditText
 import android.widget.ImageButton
 import androidx.annotation.MenuRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
+import com.google.android.material.appbar.MaterialToolbar
 
 class EditProfileActivity : AppCompatActivity() {
+
+    lateinit var toolbar: MaterialToolbar
+    lateinit var button: ImageButton
+    lateinit var fNameET: EditText
+    lateinit var nicknameET: EditText
+    lateinit var emailET: EditText
+    lateinit var locationET: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
-        val button = findViewById<ImageButton>(R.id.edit_photo)
+        toolbar = findViewById(R.id.topAppBar)
+        button = findViewById(R.id.edit_photo)
+        fNameET = findViewById(R.id.fullName_et)
+        nicknameET = findViewById(R.id.nickname_et)
+        emailET = findViewById(R.id.email_et)
+        locationET = findViewById(R.id.location_et)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         button.setOnClickListener {
             showMenu(it, R.menu.insert_photo_menu)
         }
+
+        fNameET.setText(intent.getStringExtra(ShowProfileActivity.FULL_NAME_KEY) ?: "")
+        nicknameET.setText(intent.getStringExtra(ShowProfileActivity.NICKNAME_KEY) ?: "")
+        emailET.setText(intent.getStringExtra(ShowProfileActivity.EMAIL_KEY) ?: "")
+        locationET.setText(intent.getStringExtra(ShowProfileActivity.LOCATION_KEY) ?: "")
     }
 
     private fun showMenu(v: View, @MenuRes menuRes: Int) {
@@ -41,5 +68,22 @@ class EditProfileActivity : AppCompatActivity() {
         }
         // Show the popup menu.
         popup.show()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        intent.putExtra(ShowProfileActivity.FULL_NAME_KEY, fNameET.text.toString())
+        intent.putExtra(ShowProfileActivity.NICKNAME_KEY, nicknameET.text.toString())
+        intent.putExtra(ShowProfileActivity.EMAIL_KEY, emailET.text.toString())
+        intent.putExtra(ShowProfileActivity.LOCATION_KEY, locationET.text.toString())
+        setResult(Activity.RESULT_OK, intent)
+        super.onBackPressed()
     }
 }
--- app/src/main/java/com/polito/timebanking/ShowProfileActivity.kt
+++ app/src/main/java/com/polito/timebanking/ShowProfileActivity.kt
@@ -1,12 +1,71 @@
 package com.polito.timebanking
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.appbar.MaterialToolbar
 
 class ShowProfileActivity : AppCompatActivity() {
+
+    companion object {
+        const val EDIT_KEY = 10
+        const val FULL_NAME_KEY = "group36.lab1.FULL_NAME"
+        const val NICKNAME_KEY = "group36.lab1.NICKNAME"
+        const val EMAIL_KEY = "group36.lab1.EMAIL"
+        const val LOCATION_KEY = "group36.lab1.LOCATION"
+    }
+
+    lateinit var toolbar: MaterialToolbar
+    lateinit var fNameTV: TextView
+    lateinit var nicknameTV: TextView
+    lateinit var emailTV: TextView
+    lateinit var locationTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
 
+        toolbar = findViewById(R.id.topAppBar)
+        fNameTV = findViewById(R.id.tv_fullname)
+        nicknameTV = findViewById(R.id.tv_nickname)
+        emailTV = findViewById(R.id.tv_email)
+        locationTV = findViewById(R.id.tv_location)
+
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.edit -> {
+                    editProfile()
+                    true
+                }
+
+                else -> false
+            }
+        }
+    }
+
+    private fun editProfile() {
+        val intent = Intent(this, EditProfileActivity::class.java)
+        intent.putExtra(FULL_NAME_KEY, fNameTV.text.toString())
+        intent.putExtra(NICKNAME_KEY, nicknameTV.text.toString())
+        intent.putExtra(EMAIL_KEY, emailTV.text.toString())
+        intent.putExtra(LOCATION_KEY, locationTV.text.toString())
+        startActivityForResult(intent, EDIT_KEY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            EDIT_KEY -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    fNameTV.text = data?.getStringExtra(FULL_NAME_KEY) ?: ""
+                    nicknameTV.text = data?.getStringExtra(NICKNAME_KEY) ?: ""
+                    emailTV.text = data?.getStringExtra(EMAIL_KEY) ?: ""
+                    locationTV.text = data?.getStringExtra(LOCATION_KEY) ?: ""
+                }
+            }
+            else -> Unit
+        }
     }
 }
