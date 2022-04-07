6b57f59e645dcb79cfab8a7bbaecf9b9ceaa6135
FRANCESCO FIORELLA s303922 <s303922@studenti.polito.it>
2022-04-07 18:57:44 +0200

    tutto

--- app/src/main/java/com/polito/timebanking/EditProfileActivity.kt
+++ app/src/main/java/com/polito/timebanking/EditProfileActivity.kt
@@ -1,31 +1,44 @@
 package com.polito.timebanking
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.annotation.MenuRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.PopupMenu
+import androidx.core.graphics.drawable.toBitmap
+import androidx.core.view.drawToBitmap
 import com.google.android.material.appbar.MaterialToolbar
 
 class EditProfileActivity : AppCompatActivity() {
 
     lateinit var toolbar: MaterialToolbar
+    lateinit var photoIV: ImageView
     lateinit var button: ImageButton
     lateinit var fNameET: EditText
     lateinit var nicknameET: EditText
     lateinit var emailET: EditText
     lateinit var locationET: EditText
 
+    companion object {
+        private const val REQUEST_IMAGE_CAPTURE = 1
+        private const val PHOTO_SAVE_KEY = "photo_save_key"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
         toolbar = findViewById(R.id.topAppBar)
+        photoIV = findViewById(R.id.photo)
         button = findViewById(R.id.edit_photo)
         fNameET = findViewById(R.id.fullName_et)
         nicknameET = findViewById(R.id.nickname_et)
@@ -40,10 +53,23 @@ class EditProfileActivity : AppCompatActivity() {
             showMenu(it, R.menu.insert_photo_menu)
         }
 
+        photoIV.setImageBitmap(intent.getParcelableExtra(ShowProfileActivity.PHOTO_KEY))
         fNameET.setText(intent.getStringExtra(ShowProfileActivity.FULL_NAME_KEY) ?: "")
         nicknameET.setText(intent.getStringExtra(ShowProfileActivity.NICKNAME_KEY) ?: "")
         emailET.setText(intent.getStringExtra(ShowProfileActivity.EMAIL_KEY) ?: "")
         locationET.setText(intent.getStringExtra(ShowProfileActivity.LOCATION_KEY) ?: "")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(PHOTO_SAVE_KEY, photoIV.drawable.toBitmap())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getParcelable<Bitmap>(PHOTO_SAVE_KEY)?.let {
+            photoIV.setImageBitmap(it)
+        }
     }
 
     private fun showMenu(v: View, @MenuRes menuRes: Int) {
@@ -57,15 +83,13 @@ class EditProfileActivity : AppCompatActivity() {
                     true
                 }
                 R.id.take_image -> {
-                    // Respond to context menu item 2 click.
+                    dispatchTakePictureIntent()
                     true
                 }
                 else -> false
             }
         }
-        popup.setOnDismissListener {
-            // Respond to popup being dismissed.
-        }
+
         // Show the popup menu.
         popup.show()
     }
@@ -79,11 +103,35 @@ class EditProfileActivity : AppCompatActivity() {
 
     override fun onBackPressed() {
         val intent = Intent()
-        intent.putExtra(ShowProfileActivity.FULL_NAME_KEY, fNameET.text.toString())
-        intent.putExtra(ShowProfileActivity.NICKNAME_KEY, nicknameET.text.toString())
-        intent.putExtra(ShowProfileActivity.EMAIL_KEY, emailET.text.toString())
-        intent.putExtra(ShowProfileActivity.LOCATION_KEY, locationET.text.toString())
+        val user = User(
+            fNameET.text.toString(),
+            nicknameET.text.toString(),
+            emailET.text.toString(),
+            locationET.text.toString()
+        )
+        intent.putExtra(ShowProfileActivity.FULL_NAME_KEY, user.fullName)
+        intent.putExtra(ShowProfileActivity.NICKNAME_KEY, user.nickName)
+        intent.putExtra(ShowProfileActivity.EMAIL_KEY, user.email)
+        intent.putExtra(ShowProfileActivity.LOCATION_KEY, user.location)
+        intent.putExtra(ShowProfileActivity.PHOTO_KEY, photoIV.drawable.toBitmap())
         setResult(Activity.RESULT_OK, intent)
         super.onBackPressed()
+    }
+
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        } catch (e: ActivityNotFoundException) {
+            // display error state to the user
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            photoIV.setImageBitmap(imageBitmap)
+        }
     }
 }
--- app/src/main/java/com/polito/timebanking/ShowProfileActivity.kt
+++ app/src/main/java/com/polito/timebanking/ShowProfileActivity.kt
@@ -1,37 +1,70 @@
 package com.polito.timebanking
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.graphics.drawable.toBitmap
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.gson.Gson
 
 class ShowProfileActivity : AppCompatActivity() {
 
     companion object {
         const val EDIT_KEY = 10
+        const val PHOTO_KEY = "group36.lab1.PHOTO"
         const val FULL_NAME_KEY = "group36.lab1.FULL_NAME"
         const val NICKNAME_KEY = "group36.lab1.NICKNAME"
         const val EMAIL_KEY = "group36.lab1.EMAIL"
         const val LOCATION_KEY = "group36.lab1.LOCATION"
+
+        private const val PHOTO_SAVE_KEY = "photo_save_key"
+        private const val FULL_NAME_SAVE_KEY = "photo_save_key"
+        private const val NICKNAME_SAVE_KEY = "nickname_save_key"
+        private const val EMAIL_SAVE_KEY = "email_save_key"
+        private const val LOCATION_SAVE_KEY = "location_save_key"
+
+        private const val SHARED_KEY = "shared_key"
+        private const val PROFILE_KEY = "profile"
     }
 
-    lateinit var toolbar: MaterialToolbar
-    lateinit var fNameTV: TextView
-    lateinit var nicknameTV: TextView
-    lateinit var emailTV: TextView
-    lateinit var locationTV: TextView
+    private lateinit var toolbar: MaterialToolbar
+    private lateinit var photoIV: ImageView
+    private lateinit var fNameTV: TextView
+    private lateinit var nicknameTV: TextView
+    private lateinit var emailTV: TextView
+    private lateinit var locationTV: TextView
+    private var sharedPref: SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_profile)
 
         toolbar = findViewById(R.id.topAppBar)
+        photoIV = findViewById(R.id.iv_photo)
         fNameTV = findViewById(R.id.tv_fullname)
         nicknameTV = findViewById(R.id.tv_nickname)
         emailTV = findViewById(R.id.tv_email)
         locationTV = findViewById(R.id.tv_location)
+
+        sharedPref = applicationContext?.getSharedPreferences(SHARED_KEY, Context.MODE_PRIVATE)
+
+        if (savedInstanceState == null) {
+            sharedPref?.getString(PROFILE_KEY, "")?.let {
+                if (it != "") {
+                    val user = Gson().fromJson(it, User::class.java)
+                    fNameTV.text = user.fullName
+                    nicknameTV.text = user.nickName
+                    emailTV.text = user.email
+                    locationTV.text = user.location
+                }
+            }
+        }
 
         toolbar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
@@ -45,8 +78,29 @@ class ShowProfileActivity : AppCompatActivity() {
         }
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(PHOTO_SAVE_KEY, photoIV.drawable.toBitmap())
+        outState.putString(FULL_NAME_SAVE_KEY, fNameTV.text.toString())
+        outState.putString(NICKNAME_SAVE_KEY, nicknameTV.text.toString())
+        outState.putString(EMAIL_SAVE_KEY, emailTV.text.toString())
+        outState.putString(LOCATION_SAVE_KEY, locationTV.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        savedInstanceState.getParcelable<Bitmap>(PHOTO_SAVE_KEY)?.let {
+            photoIV.setImageBitmap(it)
+        }
+        fNameTV.text = savedInstanceState.getString(FULL_NAME_SAVE_KEY) ?: ""
+        nicknameTV.text = savedInstanceState.getString(NICKNAME_SAVE_KEY) ?: ""
+        emailTV.text = savedInstanceState.getString(EMAIL_SAVE_KEY) ?: ""
+        locationTV.text = savedInstanceState.getString(LOCATION_SAVE_KEY) ?: ""
+    }
+
     private fun editProfile() {
         val intent = Intent(this, EditProfileActivity::class.java)
+        intent.putExtra(PHOTO_KEY, photoIV.drawable.toBitmap())
         intent.putExtra(FULL_NAME_KEY, fNameTV.text.toString())
         intent.putExtra(NICKNAME_KEY, nicknameTV.text.toString())
         intent.putExtra(EMAIL_KEY, emailTV.text.toString())
@@ -59,10 +113,22 @@ class ShowProfileActivity : AppCompatActivity() {
         when (requestCode) {
             EDIT_KEY -> {
                 if (resultCode == Activity.RESULT_OK) {
-                    fNameTV.text = data?.getStringExtra(FULL_NAME_KEY) ?: ""
-                    nicknameTV.text = data?.getStringExtra(NICKNAME_KEY) ?: ""
-                    emailTV.text = data?.getStringExtra(EMAIL_KEY) ?: ""
-                    locationTV.text = data?.getStringExtra(LOCATION_KEY) ?: ""
+                    data?.getParcelableExtra<Bitmap>(PHOTO_KEY)?.let {
+                        photoIV.setImageBitmap(it)
+                    }
+                    val user = User(
+                        data?.getStringExtra(FULL_NAME_KEY) ?: "",
+                        data?.getStringExtra(NICKNAME_KEY) ?: "",
+                        data?.getStringExtra(EMAIL_KEY) ?: "",
+                        data?.getStringExtra(LOCATION_KEY) ?: ""
+                    )
+                    fNameTV.text = user.fullName
+                    nicknameTV.text = user.nickName
+                    emailTV.text = user.email
+                    locationTV.text = user.location
+                    sharedPref?.edit()
+                        ?.putString(PROFILE_KEY, Gson().toJson(user) ?: "")
+                        ?.apply()
                 }
             }
             else -> Unit
