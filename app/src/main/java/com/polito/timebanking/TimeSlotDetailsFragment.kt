53409c35326b6f4daff4275e9565f9e9e021a527
FRANCESCO FIORELLA s303922 <s303922@studenti.polito.it>
2022-04-29 16:32:56 +0200

    minor fixes

--- app/src/main/java/com/polito/timebanking/TimeSlotDetailsFragment.kt
+++ app/src/main/java/com/polito/timebanking/TimeSlotDetailsFragment.kt
@@ -8,6 +8,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.polito.timebanking.databinding.FragmentTimeSlotDetailBinding
+import com.polito.timebanking.models.TimeSlot
 import com.polito.timebanking.viewmodels.TimeSlotViewModel
 
 class TimeSlotDetailsFragment : Fragment(R.layout.fragment_time_slot_detail) {
@@ -22,6 +23,7 @@ class TimeSlotDetailsFragment : Fragment(R.layout.fragment_time_slot_detail) {
     ): View {
         binding = DataBindingUtil
             .inflate(inflater, R.layout.fragment_time_slot_detail, container, false)
+        viewModel.timeslot = TimeSlot("Title", "this is a description", 2020, 5, 14, 14, 12, "Duration", "Via delle locheiscion, diesci")
         binding.timeSlot = viewModel.timeslot
 
         (activity as MainActivity).supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
