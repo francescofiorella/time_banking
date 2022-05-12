2853cc565b6428c14bb166e86bf0fcf43d35e0c0
Antonello Caputo s302879 <s302879@studenti.polito.it>
2022-05-13 00:05:22 +0200

    Profile edit working, missing skills management

--- app/src/main/java/com/polito/timebanking/viewmodels/UserViewModel.kt
+++ app/src/main/java/com/polito/timebanking/viewmodels/UserViewModel.kt
@@ -10,26 +10,31 @@ import androidx.lifecycle.MutableLiveData
 import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ListenerRegistration
-import com.polito.timebanking.models.Skill
-import com.polito.timebanking.models.SkillRepository
-import com.polito.timebanking.models.User
-import com.polito.timebanking.models.UserRepository
-import com.polito.timebanking.models.UserSkill
-import com.polito.timebanking.models.UserSkillRepository
+import com.polito.timebanking.models.*
 import kotlin.concurrent.thread
 
 class UserViewModel(application: Application) : AndroidViewModel(application) {
-    private val userRepository = UserRepository(application)
+    //private val userRepository = UserRepository(application)
+    //private val userSkillRepository = UserSkillRepository(application)
+
     private val skillRepository = SkillRepository(application)
-    private val userSkillRepository = UserSkillRepository(application)
+
+
 
     private val _user= MutableLiveData<User>()
     val user :LiveData<User> = _user
-    val currentUser: LiveData<User?> = userRepository.getUser(1)
-    val currentUserSkills: LiveData<List<Skill>> = userSkillRepository.getUserSkills(1)
+    val currentUser: LiveData<User?> = _user
+
+    private val _skill= MutableLiveData<List<Skill>>()
+    val skill : LiveData<List<Skill>> = _skill
+    val currentUserSkills: LiveData<List<Skill>> = _skill
+
     val currentUserBitmap = MutableLiveData<Bitmap>()
     val currentUserCheckedSkills = MutableLiveData<MutableList<Skill>>()
-    val allSkills: LiveData<List<Skill>> = skillRepository.getSkills()
+
+    private val _allskill= MutableLiveData<List<Skill>>()
+    val allSkills: LiveData<List<Skill>> = _allskill
+
     private val db: FirebaseFirestore
     private var l: ListenerRegistration
 
@@ -40,15 +45,23 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
                 if(e==null){
                     Log.d(TAG, "Current data: ${v?.data}")
                     _user.value = v!!.toUser()
+
                 }else{
                     Log.w(TAG, "Listen failed",e)
                     return@addSnapshotListener
-                   // _user.value = emptyList()
+                    // _user.value = emptyList()
                 }
             }
+        l= db.collection("skills").document("DefaultSkills").addSnapshotListener{
+            v,e->
+            if(e==null){
+                val skills= v?.get("skill") as ArrayList<String>
+                _allskill.value = skills!!.mapNotNull { s -> Skill(s.indexOf(s),s) }
+            }
+        }
     }
 
-    fun create(){
+    fun createUser(){
         db
             .collection("users")
             .document()
@@ -66,30 +79,42 @@ class UserViewModel(application: Application) : AndroidViewModel(application) {
             val location = get("location") as String
             val description = get("description") as String
             val photo = get("photo") as String
+            val skills = get("skills") as ArrayList<String>
+            _skill.value = skills!!.mapNotNull {s -> Skill(s.indexOf(s),s)}
             User(id,fullname, nickname,email,location,description,photo)
-
         } catch (e:Exception){
             e.printStackTrace()
             null
         }
-
     }
 
+
+
+
+
+
     fun updateUser(user: User) {
+        println(currentUserSkills.value)
         thread {
-            userRepository.updateUser(user)
+            db
+                .collection("users")
+                .document(user.id.toString())
+                .set(mapOf("description" to user.description,"email" to user.email,"nickname" to user.nickname,"fullname" to user.fullName, "location" to user.location,"photo" to user.photoPath, "skills" to (currentUserSkills.value?.map{ it.name}) ))
+                .addOnSuccessListener { Log.d("Firebase","Success") }
+                .addOnFailureListener { Log.d("Firebase",it.message?:"Error")}
+            //userRepository.updateUser(user)
         }
     }
 
     fun insertUserSkill(userSkill: UserSkill) {
         thread {
-            userSkillRepository.insertUserSkill(userSkill)
+            //userSkillRepository.insertUserSkill(userSkill)
         }
     }
 
     fun deleteUserSkill(userSkill: UserSkill) {
         thread {
-            userSkillRepository.deleteUserSkill(userSkill)
+            //userSkillRepository.deleteUserSkill(userSkill)
         }
     }
 }
