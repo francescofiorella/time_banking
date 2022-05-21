246258b1c55497e27cd362870187cf19e8b40273
Giuseppe D'Andrea <s287584@studenti.polito.it>
2022-05-21 19:12:28 +0200

    startActivityForResult replaced with registerForActivityResult

--- app/src/main/java/com/polito/timebanking/EditProfileFragment.kt
+++ app/src/main/java/com/polito/timebanking/EditProfileFragment.kt
@@ -1,6 +1,6 @@
 package com.polito.timebanking
 
-import android.app.Activity.RESULT_OK
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.graphics.Bitmap
@@ -13,6 +13,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.MenuRes
 import androidx.appcompat.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
@@ -29,10 +30,45 @@ class EditProfileFragment : Fragment() {
 
     private lateinit var binding: FragmentEditProfileBinding
 
-    companion object {
-        private const val REQUEST_CODE_SELECT_PHOTO = 100
-        private const val REQUEST_CODE_TAKE_PHOTO = 200
-    }
+    private val selectPhotoForActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == Activity.RESULT_OK) {
+                activityResult.data?.data?.let { imageUri ->
+                    var bitmap: Bitmap? = null
+                    try {
+                        bitmap = if (Build.VERSION.SDK_INT < 28) {
+                            @Suppress("DEPRECATION")
+                            MediaStore.Images.Media.getBitmap(
+                                requireContext().contentResolver,
+                                imageUri
+                            )
+                        } else {
+                            val source: ImageDecoder.Source =
+                                ImageDecoder.createSource(
+                                    requireContext().contentResolver,
+                                    imageUri
+                                )
+                            ImageDecoder.decodeBitmap(source)
+                        }
+                    } catch (e: java.lang.Exception) {
+                        Log.e("EditProfileFragment", "selectPhotoForActivityResult", e)
+                    }
+                    bitmap?.let {
+                        val rotatedBitmap = rotateBitmap(requireContext(), it)
+                        userModel.setPhoto(rotatedBitmap)
+                    }
+                }
+            }
+        }
+
+    private val takePhotoForActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { activityResult ->
+            if (activityResult.resultCode == Activity.RESULT_OK) {
+                val bitmap = activityResult.data?.extras?.get("data") as Bitmap
+                val rotatedBitmap = rotateBitmap(requireContext(), bitmap)
+                userModel.setPhoto(rotatedBitmap)
+            }
+        }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -117,58 +153,18 @@ class EditProfileFragment : Fragment() {
         val selectPhotoIntent =
             Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
         try {
-            startActivityForResult(selectPhotoIntent, REQUEST_CODE_SELECT_PHOTO)
+            selectPhotoForActivityResult.launch(selectPhotoIntent)
         } catch (e: ActivityNotFoundException) {
-            e.printStackTrace()
+            Log.e("EditProfileFragment", "dispatchSelectPhotoIntent", e)
         }
     }
 
     private fun dispatchTakePhotoIntent() {
         val takePhotoIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         try {
-            startActivityForResult(takePhotoIntent, REQUEST_CODE_TAKE_PHOTO)
+            takePhotoForActivityResult.launch(takePhotoIntent)
         } catch (e: ActivityNotFoundException) {
-            e.printStackTrace()
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == RESULT_OK) {
-            when (requestCode) {
-                REQUEST_CODE_SELECT_PHOTO -> {
-                    data?.data?.let { imageUri ->
-                        var bitmap: Bitmap? = null
-                        try {
-                            bitmap = if (Build.VERSION.SDK_INT < 28) {
-                                @Suppress("DEPRECATION")
-                                MediaStore.Images.Media.getBitmap(
-                                    requireContext().contentResolver,
-                                    imageUri
-                                )
-                            } else {
-                                val source: ImageDecoder.Source =
-                                    ImageDecoder.createSource(
-                                        requireContext().contentResolver,
-                                        imageUri
-                                    )
-                                ImageDecoder.decodeBitmap(source)
-                            }
-                        } catch (e: java.lang.Exception) {
-                            e.printStackTrace()
-                        }
-                        bitmap?.let {
-                            val rotatedBitmap = rotateBitmap(requireContext(), it)
-                            userModel.setPhoto(rotatedBitmap)
-                        }
-                    }
-                }
-                REQUEST_CODE_TAKE_PHOTO -> {
-                    val bitmap = data?.extras?.get("data") as Bitmap
-                    val rotatedBitmap = rotateBitmap(requireContext(), bitmap)
-                    userModel.setPhoto(rotatedBitmap)
-                }
-            }
+            Log.e("EditProfileFragment", "dispatchTakePhotoIntent", e)
         }
     }
 
