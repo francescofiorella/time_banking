f3411668b2b516b5d94b3cb062efb0105ca73ee3
FRANCESCO FIORELLA s303922 <s303922@studenti.polito.it>
2022-05-21 15:18:01 +0200

    minor fix

--- app/src/main/java/com/polito/timebanking/models/User.kt
+++ app/src/main/java/com/polito/timebanking/models/User.kt
@@ -1,12 +1,6 @@
 package com.polito.timebanking.models
 
-import androidx.room.Entity
-import androidx.room.Index
-import androidx.room.PrimaryKey
-
-@Entity(indices = [Index(value = ["email"], unique = true)])
 data class User(
-    @PrimaryKey(autoGenerate = true)
     val id: Int,
     var fullName: String,
     var nickname: String,
